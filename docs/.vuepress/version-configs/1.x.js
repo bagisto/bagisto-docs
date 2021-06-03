8432b79886e9b1be71b890f5d1937f6fea1d2e52
devansh bawari <devansh.bawari419@webkul.com>
2021-06-03 19:10:38 +0530

    Enhanced Requirement Section

--- docs/.vuepress/version-configs/1.x.js
+++ docs/.vuepress/version-configs/1.x.js
@@ -22,7 +22,6 @@ module.exports = [
             ['introduction/requirements', 'Requirements'],
             ['introduction/installation', 'Installation'],
             ['introduction/upgrade_to_latest_bagisto', 'Upgrade Bagisto'],
-            ['introduction/folders', 'Directory structure'],
             ['introduction/docker', 'Docker']
             
         ])
