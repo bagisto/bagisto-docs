bd5fea1215579e4a80c4d0b55e6aaf439d6359a4
Shivendra <shivendra.gupta423@webkul.in>
2024-03-06 16:00:23 +0530

    Add new feature to redirect in available page

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,34 +1,29 @@
 <template>
-  <div class="theme-container" style="padding-right: 20px;">
-    <div class="theme-default-content">
-      <img src="/logo.png" alt="Bagisto Documentation" class="logo" style="height: 86px; width: 200px;">
+    <div class="theme-container" style="padding-right: 20px;">
+      <div class="theme-default-content">
+        <img src="/logo.png" alt="Bagisto Documentation" class="logo" style="height: 86px; width: 200px;">
 
-      <h1>Page not found.</h1>
+        <h1>Page not found.</h1>
 
-      <div class="warning-container">
-        <div>
-          <strong>! WARNING</strong> You're browsing the documentation for an old version of Bagisto. Consider upgrading your project to Bagisto v2.x.
+        <div class="warning-container">
+          <div>
+            <strong>! WARNING</strong> Page Not Found
+          </div>
+
+          <div>
+            <blockquote style="color:black">{{ getMsg() }}</blockquote>
+          </div>
         </div>
 
-        <div>
-          <blockquote>{{ getMsg() }}</blockquote>
-        </div>
-      </div>
-
-      <div class="link">
-        <div>
-          <a @click="e => getCurrentUrl(e, '2.x')">
-            v2.x.(Latest)
-          </a>
-        </div>
-        <div>
-          <a @click="e => getCurrentUrl(e, '1.5.x')">
-            v1.5.x.
-          </a>
+        <div class="link">
+          <div v-for="url in availableURLs" :key="url.version">
+            <a @click="e => getCurrentUrl(e, url.objects[0])">
+              {{ url.version.replace(/\//g, '') }}
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -36,19 +31,53 @@ const msgs = [
   `This page does not exist for this version of Bagisto but was found in other versions.`,
 ]
 
+import configurations from '../../config';
+
 export default {
+  data() {
+    return {
+      currentURL: window.location.href,
+      
+      availableURLs: [],
+    }
+  },
+
+  mounted() {
+    this.findSimilarPaths(`/${this.currentURL.split('/')[3]}/`, this.currentURL.split('/')[4]);
+  },
+
   methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
     },
 
-    getCurrentUrl(e, version) {
+    getCurrentUrl(e, url) {
       e.preventDefault();
-      var currentURL = window.location.href;
 
-      var newURL = currentURL.replace('1.x', version);
+      window.location.href = url.path;
+    },
 
-      window.location.href = newURL;
+    findSimilarPaths(targetVersion, targetPath) {
+      const similarObjects = [];
+
+      let data = configurations.themeConfig.sidebar;
+
+      for (const version in data) {
+        if (version !== targetVersion) {
+          const versionData = data[version];
+
+          const matchingObjects = versionData.filter(obj => obj.path.endsWith(`/${targetPath}`));
+
+          if (matchingObjects.length > 0) {
+            similarObjects.push({
+              version,
+              objects: matchingObjects,
+            });
+          }
+        }
+      }
+
+      this.availableURLs = similarObjects;
     }
   }
 }
@@ -56,12 +85,13 @@ export default {
 
 <style scoped>
   .warning-container {
-    border: 1px solid #E9E9E9; /* Added "solid" to specify the border style and "black" for the color */
+    border: 1px solid #E9E9E9;
     border-radius: 10px;
     height: 100%;
     width: 100%;
     padding: 20px 10px 10px 10px;
     margin: 10px 0px 20px 0px;
+    background: rgb(242, 226, 196);
   }
 
   .link {
