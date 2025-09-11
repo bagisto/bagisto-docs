304d42b1e91fb07b2ef5af12925269d025062e0c
Devansh Bawari <devansh.bawari419@webkul.in>
2025-09-11 15:03:57 +0000

    fix: cleanup redirect middleware

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,6 @@
 import Layout from './Layout.vue'
-// import { setupRedirects } from './redirects'
 import './custom.css'
 
 export default {
     Layout,
-
-    // enhanceApp({ router }) {
-    //     setupRedirects(router)
-    // }
 }
